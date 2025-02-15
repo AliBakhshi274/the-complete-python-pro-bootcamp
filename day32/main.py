6e853a161656261c7b6448d64e97315adbe1e0c8
Astronaut <astronaut@pop-os.localdomain>
2025-02-15 20:13:16 +0330

    day32

--- day32/main.py
+++ day32/main.py
@@ -1,81 +1,29 @@
-# import smtplib
-#
-# my_email = "aliard.bks@gmail.com"
-# password = "kdbh fovj odwn bmhe"
-# mail_to = "polepey147@shouxs.com"
-#
-# with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs=mail_to, msg="Subject:Hello\n\nThis is the body")
-
-# import datetime as dt
-#
-# date_of_birth = dt.datetime(year=1998, month=7, day=18)
-# print(date_of_birth)
-
-import random
-import smtplib
+import pandas
 import datetime as dt
+import smtplib
+import random
 
-my_email = 'aliard.bks@gmail.com'
-password = 'kdbh fovj odwn bmhe'
-email_to = 'polepey147@shouxs.com'
-
-split_quote = []
-
-def load_quote():
-    global split_quote
-    with open("quotes.txt") as file:
-        quotes_list = file.readlines()
-        quote = random.choice(quotes_list)
-        split_quote = quote.split('-')
-def get_name():
-    return split_quote[1]
-def get_text():
-    return split_quote[0]
-def send_email():
+def send_email(to_addr, msg_body):
+    from_addr = 'aliard.bks@gmail.com'
+    password = 'kdbh fovj odwn bmhe'
     with smtplib.SMTP('smtp.gmail.com', 587) as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
-        date_time = dt.datetime.now()
-        if date_time.day == 15:
-            connection.sendmail(my_email, email_to, f"Subject:{get_name()}\n\n{get_text()}")
-
-load_quote()
-send_email()
-
+        connection.login(user=from_addr, password=password)
+        connection.sendmail(from_addr, to_addr, msg=f'Subject:Congratulations!\n\n{msg_body}')
+def get_msg_body(contact_name) -> str:
+    with open(f'letter_templates/letter_{random.randint(1, 3)}.txt', 'r') as file:
+        msg_body = file.read()
+        return msg_body.replace('[NAME]', contact_name)
 
 
+d = pandas.read_csv("birthdays.csv")
+data_list = d.to_dict(orient="records")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+today = dt.datetime.now()
+for data_dict in data_list:
+    if (data_dict['month'], data_dict['day']) == (today.month, today.day):
+        msg_body = get_msg_body(data_dict['name'])
+        send_email(data_dict['email'], msg_body)
 
 
 
